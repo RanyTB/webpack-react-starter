b2daedd3b4235ef2316310e76361470eb7bbb683
Rany Tarek Bouorm <ranytb@gmail.com>
2022-04-03 13:41:37 +0200

    feat: add support for all common image types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { getTime, sayHello } from './utils';
 import ReactLogo from './assets/react-logo.svg';
+import TestLogo from './assets/test.png';
 
 const App = () => {
   sayHello();
 
   return (
     <div>
-      <img src={ReactLogo} alt="React Logo" />
+      <div>
+        <img src={ReactLogo} alt="React Logo" />
+      </div>
+      <div>
+        <img
+          style={{ height: '48px', width: '48px' }}
+          src={TestLogo}
+          alt="React Logo"
+        />
+      </div>
       {getTime()}
     </div>
   );
